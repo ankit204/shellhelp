16b0f6d7b29ef9f8c548ab35067ec634c15207a3
ankit <ankit@cerebellumcapital.com>
2018-04-22 16:41:04 -0400

    adding paths; make with options

--- Classes/Path.java
+++ Classes/Path.java
@@ -2,8 +2,8 @@ package Classes;
 
 public class Path {
 	public static String StopwordDir="data//stopword.txt";//address of stopwords.txt
-	public static String ResultPP="data//preprocessed";//
-	
-	
-	public static String ManDir="data//man1";//address of docset.trectext
+	public static String ResultPP="data//preprocessed";
+	public static String ManDir="data//man1";
+    public static String IndexPath="data//index";
+    public static String TopicDir="data//topics.txt";
 }
--- make.sh
+++ make.sh
@@ -1,4 +1,17 @@
-rm -f PreProcessData/*.class *.class
-javac PreProcessRepo.java PreProcessData/ManCollection.java
-java PreProcessRepo
+#!/bin/bash
+
+if [ "$1" == "test" ]; then
+    echo "Runnig tests..."
+    java -cp .:lib/lucene-analyzers-common-6.3.0.jar:lib/lucene-backward-codecs-6.3.0.jar:lib/lucene-core-6.3.0.jar:lib/lucene-queryparser-6.3.0.jar runTests
+else
+    rm -f PreProcessData/*.class *.class Index/*.class Search/*.class Classes/*.class
+
+    javac -cp lib/lucene-analyzers-common-6.3.0.jar:lib/lucene-backward-codecs-6.3.0.jar:lib/lucene-core-6.3.0.jar:lib/lucene-queryparser-6.3.0.jar Index.java Indexing/*.java Classes/*.java PreProcessRepo.java PreProcessData/*.java shellhelp.java Search/*.java runTests.java
+
+    java PreProcessRepo
+
+    java -cp .:lib/lucene-analyzers-common-6.3.0.jar:lib/lucene-backward-codecs-6.3.0.jar:lib/lucene-core-6.3.0.jar:lib/lucene-queryparser-6.3.0.jar Index
+
+    java -cp .:lib/lucene-analyzers-common-6.3.0.jar:lib/lucene-backward-codecs-6.3.0.jar:lib/lucene-core-6.3.0.jar:lib/lucene-queryparser-6.3.0.jar runTests
+fi
 
